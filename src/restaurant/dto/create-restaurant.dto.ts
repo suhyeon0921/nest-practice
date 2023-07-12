20acd0ae6ad65d954184369e1590d9b8ab8700f6
suhyeon <suhyeon3484@naver.com>
2023-07-13 02:02:13 +0900

    feat: Add dto

---
+++ src/restaurant/dto/create-restaurant.dto.ts
@@ -0,0 +1,4 @@
+export class CreateRestaurantDto {
+  readonly name: string;
+  readonly category: string;
+}
---
+++ src/restaurant/dto/update-restaurant.dto.ts
@@ -0,0 +1,3 @@
+export default class UpdateRestaurantDto {
+  readonly category: string;
+}
